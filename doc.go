b17e30127c9199d3e2fe504753b98d3d5caf7673
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-11 11:15:21 -0700

    docs

--- doc.go
+++ doc.go
@@ -1,4 +1,20 @@
 /*
 Package endian provides tools for working with the Little Endian and Big Endian data (such as reading it, and writing it), or in other words, translating to and from numbers and byte sequences using Little Endian and Big Endian encodings.
+
+
+Little Endian
+
+If you are looking to translate between numbers and the little endian encoding, then use the sub-packge:
+
+	"github.com/reiver/go-endian/little"
+
+
+Big Endian
+
+If you are looking to translate between numbers and the big endian encoding, then use the sub-packge:
+
+	"github.com/reiver/go-endian/big"
+
+
 */
 package endian
