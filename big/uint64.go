b70c4d6552df49f5f635b502a48e85716edbd3b9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-11 09:20:23 -0700

    bigendian.WriteUint64To()

---
+++ big/uint64.go
@@ -0,0 +1,28 @@
+package bigendian
+
+import (
+	"io"
+)
+
+// WriteUint64To writes the big endian representation of ‘value’ into ‘writer’.
+func WriteUint64To(writer io.Writer, value uint64) (int64, error) {
+	if nil == writer {
+		return 0, errNilWriter
+	}
+
+	var b [8]byte
+
+	b[0] = uint8((0xff00000000000000 & value) >> 56)
+	b[1] = uint8((0x00ff000000000000 & value) >> 48)
+	b[2] = uint8((0x0000ff0000000000 & value) >> 40)
+	b[3] = uint8((0x000000ff00000000 & value) >> 32)
+	b[4] = uint8((0x00000000ff000000 & value) >> 24)
+	b[5] = uint8((0x0000000000ff0000 & value) >> 16)
+	b[6] = uint8((0x000000000000ff00 & value) >>  8)
+	b[7] = uint8 (0x00000000000000ff & value)
+
+	n, err := writer.Write(b[:])
+	n64 := int64(n)
+
+	return n64, err
+}
