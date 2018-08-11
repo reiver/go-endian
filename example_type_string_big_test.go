bb9ed91ecd81f4566a7d6e4322e86782660c52a1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-11 08:14:44 -0700

    created example

---
+++ example_type_string_big_test.go
@@ -0,0 +1,21 @@
+package endian_test
+
+import (
+	"github.com/reiver/go-endian"
+
+	"fmt"
+)
+
+func ExampleType_String_big() {
+
+	var endianness endian.Type = endian.Big()
+
+	s := endianness.String()
+
+	fmt.Printf("%s\n", s)
+	fmt.Printf("endianness = %s\n", endianness) // <---- implicitly calls String method.
+
+	// Output:
+	// endian.Big()
+	// endianness = endian.Big()
+}
