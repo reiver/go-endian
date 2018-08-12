95c4e847641332795b619bfb50d2329639b3a2ea
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-12 15:49:53 -0700

    added more non-exported error

--- little/errors.go
+++ little/errors.go
@@ -5,7 +5,9 @@ import (
 )
 
 var (
-	errInternalError = errors.New("Internal Error")
-	errNilReader     = errors.New("Nil Reader")
-	errNilWriter     = errors.New("Nil Writer")
+	errInternalError  = errors.New("Internal Error")
+	errNilDestination = errors.New("Nil Destination")
+	errNilReader      = errors.New("Nil Reader")
+	errNilWriter      = errors.New("Nil Writer")
+	errShortRead      = errors.New("Short Read")
 )
