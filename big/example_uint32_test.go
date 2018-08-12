c00d8ff98ce1b6d65a7f16b1c096f7f97ddcff00
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-12 10:28:58 -0700

    created example

--- big/example_uint32_test.go
+++ big/example_uint32_test.go
@@ -26,3 +26,24 @@ func ExampleWriteUint32To() {
 	// Wrote 4 bytes.
 	// Bytes: 76 54 32 10
 }
+
+func ExampleReadUint32From() {
+
+	var buffer bytes.Buffer
+	buffer.Write([]byte{0x76,0x54,0x32,0x10})
+
+	var value uint32
+
+	n64, err := bigendian.ReadUint32From(&buffer, &value)
+	if nil != err {
+		fmt.Printf("Problem writing: %s", err)
+		return
+	}
+
+	fmt.Printf("Read %d bytes.\n", n64)
+	fmt.Printf("Value: 0x%x\n", value)
+
+	// Output:
+	// Read 4 bytes.
+	// Value: 0x76543210
+}
