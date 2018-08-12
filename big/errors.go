91ba8c5f6e46ad5bfb05268b5b734f24ff7651ff
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-12 15:44:00 -0700

    handled short read error

--- big/errors.go
+++ big/errors.go
@@ -9,4 +9,5 @@ var (
 	errNilDestination = errors.New("Nil Destination")
 	errNilReader      = errors.New("Nil Reader")
 	errNilWriter      = errors.New("Nil Writer")
+	errShortRead      = errors.New("Short Read")
 )
