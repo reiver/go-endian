faf82b28fdf6a810cb4491f7def2559b24842a16
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-11 19:50:40 -0700

    docs

---
+++ native/doc.go
@@ -0,0 +1,4 @@
+/*
+Package littleendian provides tools for working with data in the native endianness (such as reading it, and writing it), or in other words, translating to and from numbers and byte sequences using the encoding of the native endianness.
+*/
+package nativeendian
