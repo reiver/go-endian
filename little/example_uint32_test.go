bcf5bfb600245afad2b090c5f73964b118339053
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-12 19:59:08 -0700

    created example

--- little/example_uint32_test.go
+++ little/example_uint32_test.go
@@ -26,3 +26,24 @@ func ExampleWriteUint32To() {
 	// Wrote 4 bytes.
 	// Bytes: 10 32 54 76
 }
+
+func ExampleReadUint32From() {
+
+	var buffer bytes.Buffer
+	buffer.Write([]byte{0x10,0x32,0x54,0x76})
+
+	var value uint32
+
+	n64, err := littleendian.ReadUint32From(&buffer, &value)
+	if nil != err {
+		fmt.Printf("Problem writing: %s", err)
+		return
+	}
+
+	fmt.Printf("Read %d bytes.\n", n64)
+	fmt.Printf("Value: 0x%x\n", value)
+
+	// Output:
+	// Read 4 bytes.
+	// Value: 0x76543210
+}
