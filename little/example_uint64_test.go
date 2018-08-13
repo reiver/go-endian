ddd15b7e8907d43c05fc44b41178de8753437207
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-12 20:15:25 -0700

    created example

--- little/example_uint64_test.go
+++ little/example_uint64_test.go
@@ -26,3 +26,24 @@ func ExampleWriteUint64To() {
 	// Wrote 8 bytes.
 	// Bytes: 10 32 54 76 98 ba dc fe
 }
+
+func ExampleReadUint64From() {
+
+	var buffer bytes.Buffer
+	buffer.Write([]byte{0x10,0x32,0x54,0x76,0x98,0xba,0xdc,0xfe})
+
+	var value uint64
+
+	n64, err := littleendian.ReadUint64From(&buffer, &value)
+	if nil != err {
+		fmt.Printf("Problem writing: %s", err)
+		return
+	}
+
+	fmt.Printf("Read %d bytes.\n", n64)
+	fmt.Printf("Value: 0x%x\n", value)
+
+	// Output:
+	// Read 8 bytes.
+	// Value: 0xfedcba9876543210
+}
