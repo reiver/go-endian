a0227736426e6ef1032dfbbf88436fd5b175d5f8
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-08-12 19:58:57 -0700

    littleendian.ReadUint32From()

--- little/uint32.go
+++ little/uint32.go
@@ -22,3 +22,31 @@ func WriteUint32To(writer io.Writer, value uint32) (int64, error) {
 
 	return n64, err
 }
+
+// ReadUint32From reads the little endian representation of ‘value’ from ‘reader’.
+func ReadUint32From(reader io.Reader, value *uint32) (int64, error) {
+	if nil == reader {
+		return 0, errNilReader
+	}
+	if nil == value {
+		return 0, errNilDestination
+	}
+
+	const length = 32/8
+
+	var b [length]byte
+
+	n, err := reader.Read(b[:])
+	n64 := int64(n)
+
+	if nil != err {
+		return n64, err
+	}
+	if length  > n {
+		return n64, errShortRead
+	}
+
+	*value = uint32(b[0]) | (uint32(b[1]) << 8) | (uint32(b[2]) << 16) | (uint32(b[3]) << 24)
+
+	return n64, nil
+}
--- little/uint32_test.go
+++ little/uint32_test.go
@@ -350,3 +350,291 @@ func TestWriteUint32To(t *testing.T) {
 		}
 	}
 }
+
+func TestReadUint32From(t *testing.T) {
+
+	tests := []struct{
+		Value []byte
+		Expected uint32
+	}{
+		{
+			Value:   []byte{0x00,0x00,0x00,0x00},
+			Expected: 0x00000000,
+		},
+		{
+			Value:   []byte{0x01,0x00,0x00,0x00},
+			Expected: 0x00000001,
+		},
+		{
+			Value:   []byte{0x02,0x00,0x00,0x00},
+			Expected: 0x00000002,
+		},
+		{
+			Value:   []byte{0x03,0x00,0x00,0x00},
+			Expected: 0x00000003,
+		},
+		{
+			Value:   []byte{0x04,0x00,0x00,0x00},
+			Expected: 0x00000004,
+		},
+		{
+			Value:   []byte{0x05,0x00,0x00,0x00},
+			Expected: 0x00000005,
+		},
+		{
+			Value:   []byte{0x06,0x00,0x00,0x00},
+			Expected: 0x00000006,
+		},
+		{
+			Value:   []byte{0x07,0x00,0x00,0x00},
+			Expected: 0x00000007,
+		},
+		{
+			Value:   []byte{0x08,0x00,0x00,0x00},
+			Expected: 0x00000008,
+		},
+		{
+			Value:   []byte{0x09,0x00,0x00,0x00},
+			Expected: 0x00000009,
+		},
+		{
+			Value:   []byte{0x0a,0x00,0x00,0x00},
+			Expected: 0x0000000a,
+		},
+		{
+			Value:   []byte{0x0b,0x00,0x00,0x00},
+			Expected: 0x0000000b,
+		},
+		{
+			Value:   []byte{0x0c,0x00,0x00,0x00},
+			Expected: 0x0000000c,
+		},
+		{
+			Value:   []byte{0x0d,0x00,0x00,0x00},
+			Expected: 0x0000000d,
+		},
+		{
+			Value:   []byte{0x0e,0x00,0x00,0x00},
+			Expected: 0x0000000e,
+		},
+		{
+			Value:   []byte{0x0f,0x00,0x00,0x00},
+			Expected: 0x0000000f,
+		},
+		{
+			Value:   []byte{0x10,0x00,0x00,0x00},
+			Expected: 0x00000010,
+		},
+		{
+			Value:   []byte{0x11,0x00,0x00,0x00},
+			Expected: 0x00000011,
+		},
+		{
+			Value:   []byte{0x12,0x00,0x00,0x00},
+			Expected: 0x00000012,
+		},
+		{
+			Value:   []byte{0x13,0x00,0x00,0x00},
+			Expected: 0x00000013,
+		},
+
+		{
+			Value:   []byte{0xef,0x00,0x00,0x00},
+			Expected: 0x000000ef,
+		},
+		{
+			Value:   []byte{0xf0,0x00,0x00,0x00},
+			Expected: 0x000000f0,
+		},
+		{
+			Value:   []byte{0xf1,0x00,0x00,0x00},
+			Expected: 0x000000f1,
+		},
+		{
+			Value:   []byte{0xf2,0x00,0x00,0x00},
+			Expected: 0x000000f2,
+		},
+		{
+			Value:   []byte{0xf3,0x00,0x00,0x00},
+			Expected: 0x000000f3,
+		},
+		{
+			Value:   []byte{0xf4,0x00,0x00,0x00},
+			Expected: 0x000000f4,
+		},
+		{
+			Value:   []byte{0xf5,0x00,0x00,0x00},
+			Expected: 0x000000f5,
+		},
+		{
+			Value:   []byte{0xf6,0x00,0x00,0x00},
+			Expected: 0x000000f6,
+		},
+		{
+			Value:   []byte{0xf7,0x00,0x00,0x00},
+			Expected: 0x000000f7,
+		},
+		{
+			Value:   []byte{0xf8,0x00,0x00,0x00},
+			Expected: 0x000000f8,
+		},
+		{
+			Value:   []byte{0xf9,0x00,0x00,0x00},
+			Expected: 0x000000f9,
+		},
+		{
+			Value:   []byte{0xfa,0x00,0x00,0x00},
+			Expected: 0x000000fa,
+		},
+		{
+			Value:   []byte{0xfb,0x00,0x00,0x00},
+			Expected: 0x000000fb,
+		},
+		{
+			Value:   []byte{0xfc,0x00,0x00,0x00},
+			Expected: 0x000000fc,
+		},
+		{
+			Value:   []byte{0xfd,0x00,0x00,0x00},
+			Expected: 0x000000fd,
+		},
+		{
+			Value:   []byte{0xfe,0x00,0x00,0x00},
+			Expected: 0x000000fe,
+		},
+		{
+			Value:   []byte{0xff,0x00,0x00,0x00},
+			Expected: 0x000000ff,
+		},
+		{
+			Value:   []byte{0x00,0x01,0x00,0x00},
+			Expected: 0x00000100,
+		},
+		{
+			Value:   []byte{0x01,0x01,0x00,0x00},
+			Expected: 0x00000101,
+		},
+		{
+			Value:   []byte{0x02,0x01,0x00,0x00},
+			Expected: 0x00000102,
+		},
+		{
+			Value:   []byte{0x03,0x01,0x00,0x00},
+			Expected: 0x00000103,
+		},
+
+		{
+			Value:   []byte{0x45,0x02,0x00,0x00},
+			Expected: 0x00000245,
+		},
+
+		{
+			Value:   []byte{0xae,0x37,0x00,0x00},
+			Expected: 0x000037ae,
+		},
+
+		{
+			Value:   []byte{0xfa,0xff,0x00,0x00},
+			Expected: 0x0000fffa,
+		},
+		{
+			Value:   []byte{0xfb,0xff,0x00,0x00},
+			Expected: 0x0000fffb,
+		},
+		{
+			Value:   []byte{0xfc,0xff,0x00,0x00},
+			Expected: 0x0000fffc,
+		},
+		{
+			Value:   []byte{0xfd,0xff,0x00,0x00},
+			Expected: 0x0000fffd,
+		},
+		{
+			Value:   []byte{0xfe,0xff,0x00,0x00},
+			Expected: 0x0000fffe,
+		},
+		{
+			Value:   []byte{0xff,0xff,0x00,0x00},
+			Expected: 0x0000ffff,
+		},
+		{
+			Value:   []byte{0x00,0x00,0x01,0x00},
+			Expected: 0x00010000,
+		},
+		{
+			Value:   []byte{0x01,0x00,0x01,0x00},
+			Expected: 0x00010001,
+		},
+		{
+			Value:   []byte{0x02,0x00,0x01,0x00},
+			Expected: 0x00010002,
+		},
+		{
+			Value:   []byte{0x03,0x00,0x01,0x00},
+			Expected: 0x00010003,
+		},
+
+		{
+			Value:   []byte{0x24,0xbd,0x57,0x13},
+			Expected: 0x1357bd24,
+		},
+
+		{
+			Value:   []byte{0xff,0xff,0xff,0xff},
+			Expected: 0xffffffff,
+		},
+	}
+
+
+	{
+		randomness := rand.New(rand.NewSource( time.Now().UTC().UnixNano() ))
+
+		const max = 50
+		for i:=0; i<max; i++ {
+			x64 := randomness.Int63n(0xffffffff)
+
+			u32 := uint32(x64)
+
+			var b0 byte = uint8( 0x000000ff & u32       )
+			var b1 byte = uint8((0x0000ff00 & u32) >>  8)
+			var b2 byte = uint8((0x00ff0000 & u32) >> 16)
+			var b3 byte = uint8((0xff000000 & u32) >> 24)
+
+			test := struct{
+				Value []byte
+				Expected uint32
+			}{
+				Value: []byte{b0,b1,b2,b3},
+				Expected:     u32,
+			}
+
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		var buffer bytes.Buffer
+
+		buffer.Write(test.Value)
+
+		var actual uint32
+
+		n64, err := littleendian.ReadUint32From(&buffer, &actual)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually go one: (%T) %q", testNumber, err, err)
+			continue
+		}
+
+		if expected, actual := int64(len(test.Value)), n64; expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
+			continue
+		}
+
+		if expected := test.Expected; expected != actual {
+			t.Errorf("For test #%d, expected 0x%X, but actually got 0x%X.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
